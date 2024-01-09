8fd77c42b81f3450a965686fe15e57f59cdbdbea
Huzefa1413 <79566904+Huzefa1413@users.noreply.github.com>
2024-01-09 16:27:05 +0500

    Navbar

--- src/pages/AdminPortal/AdminDashboard.jsx
+++ src/pages/AdminPortal/AdminDashboard.jsx
@@ -1,40 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import { useAuth } from "../../ContextAPI/Components/auth"
-import { useMediaQuery } from 'react-responsive'
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../ContextAPI/Components/auth';
+import { useMediaQuery } from 'react-responsive';
 import { Helmet } from 'react-helmet';
 import moment from 'moment';
 
-import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar, sidebarClasses } from 'react-pro-sidebar';
+import {
+  Sidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  useProSidebar,
+  sidebarClasses,
+} from 'react-pro-sidebar';
 
-import NavSidebar from '../../components/Sidebar'
-import Navbar from '../../components/Navbar'
+import NavSidebar from '../../components/Sidebar';
+import Navbar from '../../components/Navbar';
 
-import Card from '../../components/StatsCard'
+import Card from '../../components/StatsCard';
 
 function AdminDashboard() {
+  const { collapseSidebar, toggleSidebar, collapsed, toggled, broken, rtl } =
+    useProSidebar();
+  return (
+    <>
+      <div className="d-flex">
+        <div className="sidebar_div">
+          <NavSidebar
+            collapseSidebar={collapseSidebar}
+            toggleSidebar={toggleSidebar}
+            collapsed={collapsed}
+            toggled={toggled}
+            broken={broken}
+            rtl={rtl}
+          />
+        </div>
 
-    const { collapseSidebar, toggleSidebar, collapsed, toggled, broken, rtl } = useProSidebar();
-    return (
-        <>
+        <div className="page_div">
+          <Navbar
+            collapseSidebar={collapseSidebar}
+            toggleSidebar={toggleSidebar}
+            collapsed={collapsed}
+            toggled={toggled}
+            broken={broken}
+            rtl={rtl}
+          />
 
-
-
-            <div className="d-flex">
-                <div className="sidebar_div">
-                    <NavSidebar collapseSidebar={collapseSidebar} toggleSidebar={toggleSidebar} collapsed={collapsed} toggled={toggled} broken={broken} rtl={rtl} />
-                </div>
-
-                <div className="page_div">
-                    <Navbar collapseSidebar={collapseSidebar} toggleSidebar={toggleSidebar} collapsed={collapsed} toggled={toggled} broken={broken} rtl={rtl} />
-
-                    <section className=''>
-                        <h1>Admin DASHBOARD</h1>
-<Card/>
-                    </section>
-                </div>
-            </div>
-        </>
-    )
+          <section className="">
+            <h1>Admin DASHBOARD</h1>
+            <Card />
+          </section>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default AdminDashboard
+export default AdminDashboard;
