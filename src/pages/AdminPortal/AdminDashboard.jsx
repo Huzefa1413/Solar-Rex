e223f6d1e0524dbd6ddefc028255213cbeebfa33
Bilal-khan2 <bilalhamidkhan.20@gmail.com>
2024-01-10 14:53:22 +0500

    Work

--- src/pages/AdminPortal/AdminDashboard.jsx
+++ src/pages/AdminPortal/AdminDashboard.jsx
@@ -1,38 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import { useAuth } from "../../ContextAPI/Components/auth"
-import { useMediaQuery } from 'react-responsive'
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../ContextAPI/Components/auth';
+import { useMediaQuery } from 'react-responsive';
 import { Helmet } from 'react-helmet';
 import moment from 'moment';
+// import BarChart from '../../components/Charts/Charts.Students/BarChart';
 
-import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar, sidebarClasses } from 'react-pro-sidebar';
+import {
+  Sidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  useProSidebar,
+  sidebarClasses,
+} from 'react-pro-sidebar';
 
-import NavSidebar from '../../components/Sidebar'
-import Navbar from '../../components/Navbar'
+import NavSidebar from '../../components/Sidebar';
+import Navbar from '../../components/Navbar';
 
+import Card from '../../components/StatsCard';
+import Charts from '../../components/MyChart';
 
 function AdminDashboard() {
+  const { collapseSidebar, toggleSidebar, collapsed, toggled, broken, rtl } =
+    useProSidebar();
+  return (
+    <>
+      <div className="d-flex">
+        <div className="sidebar_div">
+          <NavSidebar
+            collapseSidebar={collapseSidebar}
+            toggleSidebar={toggleSidebar}
+            collapsed={collapsed}
+            toggled={toggled}
+            broken={broken}
+            rtl={rtl}
+          />
+        </div>
 
-    const { collapseSidebar, toggleSidebar, collapsed, toggled, broken, rtl } = useProSidebar();
-    return (
-        <>
+        <div className="page_div">
+          <Navbar
+            collapseSidebar={collapseSidebar}
+            toggleSidebar={toggleSidebar}
+            collapsed={collapsed}
+            toggled={toggled}
+            broken={broken}
+            rtl={rtl}
+          />
 
-
-
-            <div className="d-flex">
-                <div className="sidebar_div">
-                    <NavSidebar collapseSidebar={collapseSidebar} toggleSidebar={toggleSidebar} collapsed={collapsed} toggled={toggled} broken={broken} rtl={rtl} />
-                </div>
-
-                <div className="page_div">
-                    <Navbar collapseSidebar={collapseSidebar} toggleSidebar={toggleSidebar} collapsed={collapsed} toggled={toggled} broken={broken} rtl={rtl} />
-
-                    <section className=''>
-                        <h1>Admin DASHBOARD</h1>
-                    </section>
-                </div>
+          <section className="container-fluid py-3">
+            <div className="row">
+            <Card />
+            <Card />
+            <Card />
+            
             </div>
-        </>
-    )
+            <div>
+                {/* <BarChart/> */}
+                <Charts/>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default AdminDashboard
+export default AdminDashboard;
