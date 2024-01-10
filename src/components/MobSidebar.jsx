3995a3958235505034e2d25d472de87d0ccadcfb
saim2310 <saimrafi2310@gmail.com>
2024-01-10 13:54:59 +0500

    work

--- src/components/MobSidebar.jsx
+++ src/components/MobSidebar.jsx
@@ -14,111 +14,78 @@ import { LuLayoutDashboard } from 'react-icons/lu';
 import { LiaUniversitySolid } from 'react-icons/lia';
 import { FiChevronDown, FiUsers } from 'react-icons/fi';
 import { MdAutorenew, MdOutlineLanguage } from 'react-icons/md';
-import {
-  PiSignInBold,
-  PiSignOutBold,
-  PiUserCirclePlus,
-  PiStudent,
-} from 'react-icons/pi';
+import { PiSignInBold, PiSignOutBold, PiUserCirclePlus, PiStudent } from 'react-icons/pi';
 
-import logo from '../assets/logosolarrex.png';
-import logo2 from '../assets/logo-icon.png';
-import germany_flag from '../assets/language icons/germany.png';
-import england_flag from '../assets/language icons/united-kingdom.png';
+import logo from "../assets/logo.png";
+import logo2 from "../assets/logo-icon.png";
+import germany_flag from "../assets/language icons/germany.png";
+import england_flag from "../assets/language icons/united-kingdom.png";
 
 import Collapse from 'react-bootstrap/Collapse';
 
+
 function MobSidebar() {
-  const navigate = useNavigate();
-  const { user, setUser } = useAuth();
-  const { handlePathname, style } = usePathname();
-  const [cookies, setCookie, removeCookie] = useCookies(['pk2']);
+    const navigate = useNavigate();
+    const { user, setUser } = useAuth();
+    const { handlePathname, style } = usePathname();
+    const [cookies, setCookie, removeCookie] = useCookies(["pk2"]);
 
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  function signout() {
-    removeCookie('pk2');
-    setUser(null);
-  }
+    function signout() {
+        removeCookie("pk2")
+        setUser(null)
+    }
 
-  return (
-    <>
-      {/* <img src={logo2} alt="logo" className='img-fluid me-1' style={{ width: "50px" }} /> */}
-      <button
-        className="btn nav_sidebar_toggle_btn px-0"
-        type="button"
-        data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasWithBothOptions"
-        aria-controls="offcanvasWithBothOptions"
-      >
-        <HiBars3 className="nav_sidebar_toggle_icon" />
-      </button>
 
-      <div
-        className="offcanvas offcanvas-start mob_sidebar"
-        data-bs-scroll="true"
-        tabindex="-1"
-        id="offcanvasWithBothOptions"
-        aria-labelledby="offcanvasWithBothOptionsLabel"
-      >
-        <div className="offcanvas-header d-flex ai-center">
-          <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">
-            {/* <img src={logo} alt="logo" className='img-fluid' /> */}
-          </h5>
-          <button
-            type="button"
-            className="btn btn-close"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          >
-            <AiOutlineClose />
-          </button>
-        </div>
-        <div className="offcanvas-body mob_sidebar_body">
-          <ul>
-            <>
-              <li className="py-3">
-                <img
-                  src={logo}
-                  className="img-fluid"
-                  style={{ width: '150px' }}
-                />
-              </li>
+    return (
+        <>
+            {/* <img src={logo2} alt="logo" className='img-fluid me-1' style={{ width: "50px" }} /> */}
+            <button className="btn nav_sidebar_toggle_btn px-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
+                <HiBars3 className='nav_sidebar_toggle_icon' />
+            </button>
 
-              <li className={`${style == 'sign_in' ? 'active' : 'unactive'}`}>
-                <Link
-                  to="/sign-in"
-                  className={`${style == 'sign_in' ? 'activeLink' : 'link'}`}
-                >
-                  <div className="d-flex ai-center">
-                    <PiSignInBold className="me-2 link_icon" />
-                    <p>Sign In</p>
-                  </div>
-                </Link>
-              </li>
-            </>
-            <>
-              <li
-                className={`${
-                  style == 'admin_dashboard' ? 'active' : 'unactive'
-                }`}
-              >
-                <Link
-                  to="/admin-dashboard"
-                  className={`${
-                    style == 'admin_dashboard' ? 'activeLink' : 'link'
-                  }`}
-                >
-                  <div className="d-flex ai-center">
-                    <LuLayoutDashboard className="me-2 link_icon" />
-                    <p>Dashboard</p>
-                  </div>
-                </Link>
-              </li>
-            </>
-          </ul>
+            <div className="offcanvas offcanvas-start mob_sidebar" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+                <div className="offcanvas-header d-flex ai-center">
+                    <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">
+                        {/* <img src={logo} alt="logo" className='img-fluid' /> */}
+                    </h5>
+                    <button type="button" className="btn btn-close" data-bs-dismiss="offcanvas" aria-label="Close">
+                        <AiOutlineClose />
+                    </button>
+                </div>
+                <div className="offcanvas-body mob_sidebar_body">
+                    <ul>
+                        <>
 
-          {/* <div className="mob_language_show">
+
+                            <li className={`${style == "sign_in" ? "active" : "unactive"}`}>
+                                <Link to="/sign-in" className={`${style == "sign_in" ? "activeLink" : "link"}`}>
+                                    <div className='d-flex ai-center'>
+                                        <PiSignInBold className='me-2 link_icon' />
+                                        <p>Sign In</p>
+                                    </div>
+                                </Link>
+                            </li>
+
+                        </>
+                        <>
+
+                            <li className={`${style == "admin_dashboard" ? "active" : "unactive"}`}>
+                                <Link to="/admin-dashboard" className={`${style == "admin_dashboard" ? "activeLink" : "link"}`}>
+                                    <div className='d-flex ai-center'>
+                                        <LuLayoutDashboard className='me-2 link_icon' />
+                                        <p>Dashboard</p>
+                                    </div>
+                                </Link>
+                            </li>
+
+                        </>
+
+
+                    </ul>
+
+                    {/* <div className="mob_language_show">
                         <div className="btn-group dropup">
                             <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                                 <MdOutlineLanguage className='language_icon' />
@@ -138,10 +105,10 @@ function MobSidebar() {
                             </ul>
                         </div>
                     </div> */}
-        </div>
-      </div>
-    </>
-  );
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default MobSidebar;
+export default MobSidebar
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,147 +1,147 @@
-import { useEffect, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { useAuth } from '../ContextAPI/Components/auth';
+import { useEffect, useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useAuth } from "../ContextAPI/Components/auth"
 
-import LOGO from '../assets/logo.png';
+import LOGO from "../assets/logo.png"
 
-import profile from '../assets/profile.svg';
-import avatar from '../assets/avatar.jpg';
+import profile from "../assets/profile.svg"
 
-import MobSidebar from './MobSidebar';
-import { PiGraduationCap, PiSignOutBold } from 'react-icons/pi';
-import { useCookies } from 'react-cookie';
+import MobSidebar from './MobSidebar'
+import { PiGraduationCap, PiSignOutBold } from 'react-icons/pi'
+import { useCookies } from 'react-cookie'
+
 
 import { HiOutlineUserCircle } from 'react-icons/hi2';
 import { MdOutlineLock } from 'react-icons/md';
 // import { profilePicUrl } from '../helpers/data';
 // import { User_Profile_Pic } from './Profile/upload_profilePic'
-import { useMediaQuery } from 'react-responsive';
+import { useMediaQuery } from 'react-responsive'
 
-import './components.css';
+
 function Navbar() {
-  const { user, Logout } = useAuth();
 
-  const { hash } = useLocation();
-  const navigate = useNavigate();
+    const { user, Logout } = useAuth()
 
-  const [showForm, setShowForm] = useState('');
+    const { hash } = useLocation();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    setShowForm(hash.split('#')[1] || 'instructions');
-  }, [hash]);
+    const [showForm, setShowForm] = useState("");
 
-  const isMobile = useMediaQuery({ query: '(max-width: 1200px)' });
+    useEffect(() => {
+        setShowForm((hash.split("#"))[1] || "instructions");
+    }, [hash]);
 
-  return (
-    <>
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <div className={`${isMobile ? 'd-none' : 'd-block'}`}>
-          {showForm == 'demographics_information' ||
-          showForm == 'academic_background' ||
-          showForm == 'financial_information' ||
-          showForm == 'additional_information' ||
-          showForm == 'sop' ||
-          showForm == 'preview' ||
-          showForm == 'final' ||
-          showForm == 'renewal_details' ? (
-            <Link to="/dashboard" className="link">
-              <img src={LOGO} alt="logo" className="img-fluid" />
-            </Link>
-          ) : (
-            ''
-          )}
-        </div>
+    const isMobile = useMediaQuery({ query: '(max-width: 1200px)' });
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <div class="nav-item navbar-brand nav_sidebar_toggle">
-            <MobSidebar />
-          </div>
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg navbar-light">
 
-          <div class="nav-item form-inline">
-            {/* <DarkMode /> */}
-
-            <div className={`${isMobile ? 'd-none' : 'd-block'}`}>
-              {showForm == 'demographics_information' ||
-              showForm == 'academic_background' ||
-              showForm == 'financial_information' ||
-              showForm == 'additional_information' ||
-              showForm == 'sop' ||
-              showForm == 'preview' ||
-              showForm == 'final' ||
-              showForm == 'renewal_details' ? (
-                <div className="me-2">
-                  <Link to="/dashboard" className="link">
-                    <button className="btn goto_dahboard_btn">Dashboard</button>
-                  </Link>
+                <div className={`${isMobile ? "d-none" : "d-block"}`}>
+                    {
+                        (showForm == "demographics_information") || (showForm == "academic_background") || (showForm == "financial_information") || (showForm == "additional_information") || (showForm == "sop") || (showForm == "preview") || (showForm == "final") || (showForm == "renewal_details") ?
+                            <Link to="/dashboard" className='link'>
+                                <img src={LOGO} alt="logo" className='img-fluid' />
+                            </Link>
+                            :
+                            ""
+                    }
                 </div>
-              ) : (
-                ''
-              )}
-            </div>
 
-            <div className="nav-item __account">
-              <div className="account d-flex ai-center jc-end">
-                <div className="img">
-                  {/* <User_Profile_Pic classes={"sm mx-auto mb-0"} /> */}
-                  <img src={avatar} alt="" />
-                  {/* <h2>image</h2> */}
-                </div>
-                {/* <div className='ml-3'>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="nav-item navbar-brand nav_sidebar_toggle" >
+                        <MobSidebar />
+                    </div>
+
+                    <div class="nav-item form-inline">
+
+                        {/* <DarkMode /> */}
+
+                        <div className={`${isMobile ? "d-none" : "d-block"}`}>
+                            {
+                                (showForm == "demographics_information") || (showForm == "academic_background") || (showForm == "financial_information") || (showForm == "additional_information") || (showForm == "sop") || (showForm == "preview") || (showForm == "final") || (showForm == "renewal_details") ?
+                                    <div className='me-2'>
+                                        <Link to="/dashboard" className='link'>
+                                            <button className='btn goto_dahboard_btn'>Dashboard</button>
+                                        </Link>
+                                    </div>
+                                    :
+                                    ""
+                            }
+                        </div>
+
+                        <div className="nav-item __account">
+
+                            <div className="account d-flex ai-center jc-end">
+                                <div className='img'>
+                                    {/* <User_Profile_Pic classes={"sm mx-auto mb-0"} /> */}
+                                    <h2>image</h2>
+                                </div>
+                                {/* <div className='ml-3'>
                                     <h6 className='mb-0 limit-text xs'>{user?.fullName || "Username"}</h6>
                                 </div> */}
-              </div>
+                            </div>
 
-              <div className="profile_box">
-                <div className="user_profile_info">
-                  <div className="mb-2">
-                    <Link to="/university-profile" className="link">
-                      <HiOutlineUserCircle className="logOut_icon" />
-                      Profile
-                    </Link>
-                  </div>
-                  <div>
-                    <Link to="/university-profile#password" className="link">
-                      <MdOutlineLock className="logOut_icon" />
-                      Password
-                    </Link>
-                  </div>
+
+                            <div className='profile_box'>
+
+
+                                <div className='user_profile_info'>
+                                    <div className='mb-2'>
+                                        <Link to="/university-profile" className="link">
+                                            <HiOutlineUserCircle className="logOut_icon" />
+                                            Profile
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        <Link to="/university-profile#password" className="link">
+                                            <MdOutlineLock className="logOut_icon" />
+                                            Password
+                                        </Link>
+                                    </div>
+                                </div>
+
+
+
+                                <div className='user_profile_info'>
+                                    <div className='mb-2'>
+                                        <Link to="/profile" className="link">
+                                            <HiOutlineUserCircle className="logOut_icon" />
+                                            Profile
+                                        </Link>
+                                    </div>
+                                    <div className='mb-2'>
+                                        <Link to="/profile#qualification" className="link">
+                                            <PiGraduationCap className="logOut_icon" />
+                                            Qualification
+                                        </Link>
+                                    </div>
+
+                                    <div>
+                                        <Link to="/profile#password" className="link">
+                                            <MdOutlineLock className="logOut_icon" />
+                                            Password
+                                        </Link>
+                                    </div>
+                                </div>
+
+
+                                <div className='logout_box'>
+                                    <Link to="#" className="link" onClick={() => Logout()}>
+                                        <PiSignOutBold className="logOut_icon" />
+                                        Log Out
+                                    </Link>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
+            </nav>
 
-                <div className="user_profile_info">
-                  <div className="mb-2">
-                    <Link to="/profile" className="link">
-                      <HiOutlineUserCircle className="logOut_icon" />
-                      Profile
-                    </Link>
-                  </div>
-                  <div className="mb-2">
-                    <Link to="/profile#qualification" className="link">
-                      <PiGraduationCap className="logOut_icon" />
-                      Qualification
-                    </Link>
-                  </div>
-
-                  <div>
-                    <Link to="/profile#password" className="link">
-                      <MdOutlineLock className="logOut_icon" />
-                      Password
-                    </Link>
-                  </div>
-                </div>
-
-                <div className="logout_box">
-                  <Link to="#" className="link" onClick={() => Logout()}>
-                    <PiSignOutBold className="logOut_icon" />
-                    Log Out
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+        </>
+    )
 }
 
-export default Navbar;
+
+export default Navbar
