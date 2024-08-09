7fbdfb7f64f6e95bd65784dc4098e60815d4f876
Huzefa1413 <79566904+Huzefa1413@users.noreply.github.com>
2024-08-09 23:42:15 +0500

    Api

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-const API_URL = "http://localhost:4000"
-export { API_URL }
+// const API_URL = 'http://localhost:4000';
+const API_URL = 'https://3a1b-175-107-220-139.ngrok-free.app';
+
+export { API_URL };
